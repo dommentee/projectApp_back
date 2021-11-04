dcc99c9c05b71a1aab25481a2d02c0bb2735b182
dommentee <chuku0929@gmail.com>
2021-11-04 19:23:35 -0400

    added code for server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+//express//mongoose//cors/
+const express = require('express');
+const mongoose = require('mongoose')//for database
+const cors = require('cors')
+
+// middlewaregit
+const app = express();
+app.use(cors())
+const PORT = 3001//react runs on 3000
+
+app.use(express.json())//call express.json for data
+
+app.get('/', (req, res) => {
+  res.send("post app")
+})
+
+//controllers
+// const postsController = require('./controllers/post.js')
+// app.use('/posts', postsController)
+  
+app.listen(PORT, () => {
+  console.log('listening to', PORT)
+})
+
+mongoose.connect('mongodb://localhost:27017/posts')
+mongoose.connection.once('open', ()=>{
+  console.log('connected to mongod...');
+});
